52fe828756a6ca4d7d99cce16696735915bc2470
Afjalul Karim Saad <aksaad.wd@gmail.com>
2021-08-23 17:58:42 +0600

    Full-Function-Solving

--- js/bank.js
+++ js/bank.js
@@ -1,4 +1,4 @@
-//Depositing money
+/* //Depositing money
 document.getElementById("deposit-btn").addEventListener("click", function () {
     const depositInput = document.getElementById("deposit-input");
     const newDepositAmount = depositInput.value;
@@ -50,4 +50,63 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
     }
     //Clearing the input field
     withdrawInput.value = "";
+}); */
+
+
+//Global Variables
+const depositTotal = document.getElementById("deposit-total");
+const depositInput = document.getElementById("deposit-input");
+const depositBtn = document.getElementById("deposit-btn");
+
+const withdrawTotal = document.getElementById("withdraw-total");
+const withdrawInput = document.getElementById("withdraw-input");
+const withdrawBtn = document.getElementById("withdraw-btn");
+
+const balanceTotal = document.getElementById("balance-total");
+
+//Deposit and Withdraw Function
+function updateTotal(totalFieldAmount, fieldInput) {
+    const previousTotal = parseFloat(totalFieldAmount.innerText);
+    const newTotal = previousTotal + parseFloat(fieldInput.value);
+    if (fieldInput.value == "") {
+        fieldInput.value = 0;
+        alert("Please, fill in the field!");
+    }
+    else if (fieldInput.value < 1) {
+        fieldInput.value = 0;
+        alert("Please, enter a valid number!");
+    }
+    else {
+        if (withdrawInput.value < parseFloat(balanceTotal.innerText)) {
+            totalFieldAmount.innerText = newTotal;
+        }
+    }
+    // return newTotal;
+}
+function updateBalance(totalFieldAmount, isAdding) {
+    if (isAdding == true) {
+        balanceTotal.innerText = parseFloat(balanceTotal.innerText) + parseFloat(totalFieldAmount.value);
+        depositInput.value = "";
+    }
+    else {
+        if (withdrawInput.value < parseFloat(balanceTotal.innerText)) {
+            balanceTotal.innerText = parseFloat(balanceTotal.innerText) - parseFloat(totalFieldAmount.value);
+            withdrawInput.value = "";
+        }
+        else {
+            alert("You can't withdraw more than the balance!");
+            withdrawInput.value = "";
+        }
+    }
+}
+
+//Depositing
+depositBtn.addEventListener("click", function () {
+    updateTotal(depositTotal, depositInput);
+    updateBalance(depositInput, true);
+});
+//Withdraw
+withdrawBtn.addEventListener("click", function () {
+    updateTotal(withdrawTotal, withdrawInput);
+    updateBalance(withdrawInput, false);
 });
